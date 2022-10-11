8e9848e90b7c08e4f3f41c79839a1a734cdf44cc
Edvinas Mikalajunas <edvinas.mikalajunas@gmail.com>
2022-10-11 09:05:17 +0300

    10: edited edit_record.php

--- app/includes/edit_record.php
+++ app/includes/edit_record.php
@@ -1,10 +1,6 @@
 <?php 
-
 //connecting classes and functions
 require_once 'init.php';
-
-//authentification
-//if(!$session->is_signed_in()){redirect('../includes/logout.php');}
 
 //gets form DATA URL, updates, forwards to index
 if(isset($_POST['submit'])){
@@ -28,7 +24,6 @@ redirect('index.php');
 //gets record DATA from db and sets to placeholders
 $records = Records::find_by_id($_GET['id']);
 ?>
-
 
 <!--START of add record form-->
 <div class="row">
@@ -63,23 +58,6 @@ $records = Records::find_by_id($_GET['id']);
                         <label for="title"><?php echo _ORDER_LAYOUT_NAME; ?></label>
                         <input type="text" class="form-control" name="record_layout_name" value="<?php echo $records->record_layout_name; ?>">
                     </div>
-                    
-                    
-                    
-<!--
-                    <div class="form-group">
-                       <label for="title">Rulono plotis</label><br>
-                       <select name="record_material_width" id="record_material_width">
-                           <option value="1.37">----------------------------</option>
-                           <option value="0.91">0.91 m</option>
-                           <option value="1.10">1.10 m</option>
-                           <option value="1.27">1.27 m</option>
-                           <option value="1.37">1.37 m</option>
-                           <option value="1.50">1.50 m</option>
-                           <option value="1.60">1.60 m</option>
-                       </select>
-                    </div>
--->
                     <div class="mb-1">
                         <label for="title"><?php echo _ORDER_QUANTITY; ?></label>
                         <input type="text" class="form-control" id="parameter_3" name="record_quantity" value="<?php echo $records->record_quantity; ?>">
@@ -93,96 +71,6 @@ $records = Records::find_by_id($_GET['id']);
                         <label for="title"><?php echo _RECORD_DIMENSION_2; ?></label>
                         <input type="text" class="form-control" id="parameter_2" name="record_dimension2" value="<?php echo $records->record_dimension2; ?>">
                     </div>
-<!--
-                   <div class="form-group">
-                        <label for="title">Kaina už kv.m</label>
-                        <input type="text" class="form-control_addrecord" id="parameter_4" name="record_price_of_sqm">
-                    </div>
--->
-<!--
-                    <div class="form-group">
-                         <label for="title">Sąmata</label>
-                        <input type="text" class="form-control_addrecord" id="estimateResult" value="0" name="record_estimate">
-                    </div>
--->
-
-
-        <!--Estimate SCARPS calculation START-->
-
-        <?php
-//
-//        //example task: measurements 50x80 cm, quantity 5 units.
-//
-//        if (isset($_POST['add_record'])){
-//        $record_material_roll_width = $_POST['record_material_width']; //example 1.6
-//
-//        $record_quanity_in_line_1 = $record_material_roll_width / $record_dimension1; //result fits 3
-//        $record_quanity_in_line_2 = $record_material_roll_width / $record_dimension2; //result fits 2
-//
-//        //if($record_quanity_in_line_1 < $record_quanity_in_line_2) {
-//        if(1 < 2) {
-//
-//        ////checks how much records fits in one whole role string
-//        //$check_2 = $record_dimension2 * $record_dimension2 * $record_quanity_in_line_2;
-//        //
-//        ////first scrap
-//        //$record_scarp_1 = $record_material_roll_width - $check_2;
-//        //
-//        //$record_whole_blocks_without_scarp = $record_quantity / $record_quanity_in_line_2;
-//        //$record_whole_blocks_without_scarp = round($record_whole_blocks_without_scarp);
-//        //
-//        ////coounts whole blocks lentgh
-//        //$record_whole_blocks_without_scarp_length = $record_whole_blocks_without_scarp * $record_dimension2;
-//        //
-//        ////counts last scrap
-//        //$record_scrap_2 = $record_dimension1 * $record_dimension2 * ($record_quantity - ($record_whole_blocks_without_scarp * $record_quanity_in_line_1));
-//        //
-//        //$record_scrap_total = $record_scarp_1 + $record_scrap_2;
-//        //
-//        //}
-//        //else {
-//
-//        //0.8 m
-//
-//        //checks how much records fits in one whole role string
-//        $check_1 = $record_dimension1 * $record_quanity_in_line_1;
-//
-//        //first scrap
-//        $record_scarp_1 = $record_material_roll_width - $check_1;
-//
-//        $record_whole_blocks_without_scarp = $record_quantity / $record_quanity_in_line_1;
-//
-//        //counts whole blocks
-//        $record_whole_blocks_without_scarp = round($record_whole_blocks_without_scarp);
-//
-//        //coounts whole blocks lentgh
-//        $record_whole_blocks_without_scarp_length = $record_whole_blocks_without_scarp * $record_dimension1;
-//
-//        //counts last scrap
-//        $record_scrap_2 = $record_dimension1 * $record_dimension2 * ($record_quantity - ($record_whole_blocks_without_scarp * $record_quanity_in_line_1));
-//
-//        $record_scrap_total = $record_quanity_in_line_1;
-//
-//
-//
-//        //sets DATA to record_material_scrap db
-//        $query = "UPDATE records SET record_material_scrap='$record_scrap_total'";
-//        if (mysqli_query($conSecure, $query)) {
-//        echo "New record created successfully";
-//        } else {
-//        echo "Error: " . $query . "<br>" . mysqli_error($conSecure);
-//        }
-//
-//        }
-//        }
-
-        ?>
-        <!--Estimate SCARPS calculation END          -->
-
-
-        <!--Estimate PRICE calculator START-->
-        <!--Estimate caclualtion script START-->
-
         <script type="text/javascript">
 
                     function estimateCalculation() {
@@ -190,16 +78,8 @@ $records = Records::find_by_id($_GET['id']);
                       var b = document.getElementById("parameter_2").value;
                       var c = document.getElementById("parameter_3").value;
                       var d = document.getElementById("parameter_4").value;
-        //              var e = document.getElementById("record_material_width").value;
                       var f = document.getElementById('record_transportation').value;
-
-        //            if (typeof f === 'undefined') {
-        //
-        //            } else { f = 4;}
-
-        //              alert(a);alert(b);alert(c);alert(d);alert(f);
                       var z = a * b * c * d + +f;
-        //              var z = z + f;
                       alert("Šio gaminio kaina:" + " " + z + " " + "eur be PVM");
 
 
@@ -209,24 +89,6 @@ $records = Records::find_by_id($_GET['id']);
 
         </script>
 
-        <!--Estimate caclualtion script END-->
-
-<!--
-                    <div class="form-group">
-                        <label for="post_status">Apdirbimo informacija </label>
-                        <input type="text" class="form-control_addrecord" name="record_option1">
-                    </div>
-
-                    <div class="form-group">
-                       <label for="title">Pristatymas</label><br>
-                       <select name="record_logistics" id="record_transportation">
-                           <option value="0">----------------------------</option>
-                           <option value="0">Klientas pasiima pats</option>
-                           <option value="4" id="">Siunčiame su VENIPAK</option>
-                           <option value="4" id="">Siunčiame su DPD</option>
-                       </select>
-                    </div>
--->
 <!--Record notes slider modul START-->
                     <div class="mb-4">                      
                         <label id="toggle" for="post_image" class='dropdown-call arrow-down'><?php echo _ORDER_NOTES; ?></label>                                                               <input type="text" class="form-control-notesbox" name="record_notes" value="<?php echo $records->record_notes; ?>">
