2c74f029514a0aea9bcd3a0c19cf13e1d1a9c7f9
Edvinas Mikalajunas <edvinas.mikalajunas@gmail.com>
2022-10-11 09:01:09 +0300

    9: edited config.php

--- app/includes/config.php
+++ app/includes/config.php
@@ -5,10 +5,5 @@ define('DB_USER','root');
 define('DB_PASSWORD','');
 define('DB_NAME','emsis1.5_db');
 
-////database constants REMOTE connection 
-//define('DB_HOST','localhost');
-//define('DB_USER','addparklt_emsisoop');
-//define('DB_PASSWORD','e27PQA44ydvmt5C7');
-//define('DB_NAME','addparklt_emsisoop');
 
 ?>
